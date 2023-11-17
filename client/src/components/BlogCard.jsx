b4d86d1ec0caadd7c5a51c485d42608f7e5d61ba
shivaprasadnashte <nashted.ug21.ee@nitp.ac.in>
2023-11-17 21:37:54 +0530

    minor fix

--- client/src/components/BlogCard.jsx
+++ client/src/components/BlogCard.jsx
@@ -2,27 +2,27 @@ import React from 'react'
 
 function BlogCard() {
     return (
-   <>
+        <>
             <div>
                 <div className=' bg-yellow-500  w-full'>
                     <img src="/public/images/dd.jpg" alt="" className=' w-64' />
                     <div className=' p-3 w-64 flex flex-col items-center justify-center'>
                         <p>catigories</p>
                         <p className=' text-xl font-bold'>Blog Title</p>
-                      
+
                         <div className=' flex justify-between'>
                             <p className=' text-xs'>Author</p>
                         </div>
                         <div >
-                        <p className=' text-sm text-left overflow-auto'>Lorem lo ipsum dolor sit amet consectetur adipisicing elit. Quia, voluptatem.</p>
+                            <p className=' text-sm text-left overflow-auto'>Lorem loclear ipsum dolor sit amet consectetur adipisicing elit. Quia, voluptatem.</p>
                         </div>
 
                     </div>
                 </div>
-                </div >
+            </div >
 
-            </>
-            )
+        </>
+    )
 }
 
-            export default BlogCard
+export default BlogCard
